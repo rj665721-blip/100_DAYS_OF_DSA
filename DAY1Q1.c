085cb2d7c2b028455025a1fb3745d6e039dea6ba
rj665721-blip <rj665721@gmail.com>
2026-02-08 22:45:49 +0530

    Add files via upload

---
+++ DAY1Q1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, pos, x;
+
+    if (scanf("%d", &n) != 1) return 0;
+
+    int arr[n + 1];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &pos);
+    scanf("%d", &x);
+
+    for (int i = n; i >= pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[pos - 1] = x;
+
+    for (int i = 0; i <= n; i++) {
+        printf("%d%s", arr[i], (i == n) ? "" : " ");
+    }
+
+    return 0;
+}
